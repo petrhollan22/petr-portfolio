3c74d07e212d700fb7528b3937fb57681874c526
petrhollan22 <petr.hollan22@gmail.com>
2026-08-07 12:51:43 +0000

    Exclude .well-known from middleware

--- middleware.ts
+++ middleware.ts
@@ -4,5 +4,5 @@ import { routing } from './i18n/routing';
 export default createMiddleware(routing);
 
 export const config = {
-  matcher: ['/', '/(cs|en)/:path*', '/((?!api|_next|_vercel|.*\\..*).*)'],
+  matcher: ['/', '/(cs|en)/:path*', '/((?!api|_next|_vercel|\\.well-known|.*\\..*).*)'],
 };
