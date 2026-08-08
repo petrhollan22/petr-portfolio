d561d8fa9de5d80f042d374faf74428072bdca65
petrhollan22 <petr.hollan22@gmail.com>
2026-08-08 12:58:53 +0000

    Exclude studio from i18n middleware

--- middleware.ts
+++ middleware.ts
@@ -4,5 +4,5 @@ import { routing } from './i18n/routing';
 export default createMiddleware(routing);
 
 export const config = {
-  matcher: ['/', '/(cs|en)/:path*', '/((?!api|_next|_vercel|\\.well-known|.*\\..*).*)'],
+  matcher: ['/', '/(cs|en)/:path*', '/((?!api|studio|_next|_vercel|.well-known|.*\..*).*)'],
 };
